03b871ab51087d69bd1f199324ff7026885a6aac
Cahyanto Setya Budi <cahyantosetyabudi@gmail.com>
2020-09-27 14:49:50 +0700

    Initial Commit

---
+++ SampleAVPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleAVPlayer
+//
+//  Created by Cahyanto Setya Budi on 27/09/20.
+//  Copyright © 2020 Cahyanto Setya Budi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
